caf02463fc517de0ea6e044ce6df4b40d9615a6b
caljshim <caljshim@gmail.com>
2024-10-28 22:49:35 -0400

    first commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,58 +12,6 @@ const Navbar = () => {
 
 
   return (
-    // <div className="h-screen bg-darkblue">
-    //   <div className="w-full flex flex-col h-full items-center p-4">
-    //     <div className="flex flex-row text-left mt-4 items-center justify-between">
-    //       <Link
-    //         to="/"
-    //         className="text-2xl sm:text-3xl font-bold uppercase bg-clip-text"
-    //       >
-    //         <MdPerson size={80} color="white"/>
-    //       </Link>
-    //       <div className="flex flex-col text-white">
-    //         <span>
-    //           Caleb Shim
-    //         </span>
-    //         <span>
-    //           {user?.data.email}
-    //         </span>
-    //         <span>
-    //           Spalding Cove
-    //         </span>
-    //       </div>
-    //     </div>
-    //     <button onClick={handleClick}>
-    //       Log Out
-    //     </button>
-    //     <div className="flex flex-col items-center mt-4 space-y-4 w-full">
-    //       <Link to="/create" className="w-5/6">
-    //         <button className="w-full py-4 bg-lightgray rounded-lg">
-    //           <div className="flex flex-row justify-between px-4">
-    //             <span className="mr-2 text-darkgray">Sell</span>
-    //             <CiSquarePlus className="text-2xl" color="darkgray" />
-    //           </div>
-    //         </button>
-    //       </Link>
-    //       <Link to="/" className="w-5/6">
-    //         <button className="w-full py-4 bg-lightgray rounded-lg">
-    //           <div className="flex flex-row justify-between px-4">
-    //             <span className="mr-2 text-darkgray">Browse</span>
-    //             <CiSquarePlus className="text-2xl" color="darkgray" />
-    //           </div>
-    //         </button>
-    //       </Link>
-    //       <Link to="/messages" className="w-5/6">
-    //         <button className="w-full py-4 bg-lightgray rounded-lg">
-    //           <div className="flex flex-row justify-between px-4">
-    //             <span className="mr-2 text-darkgray">Messages</span>
-    //             <CiSquarePlus className="text-2xl" color="darkgray" />
-    //           </div>
-    //         </button>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
 
     <div className='fixed inset-x-0 bottom-6 flex justify-center'>
       <div className='flex flex-row justify-center space-x-8 backdrop-blur-md bg-opacity-30 bg-dark-blue w-[75%] rounded-full py-2 border border-white border-opacity-40'>
