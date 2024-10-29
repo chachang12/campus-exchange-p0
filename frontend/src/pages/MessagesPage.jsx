44ea50f9f00e03c150acf7e67cb9cf32cf6e3612
caljshim <caljshim@gmail.com>
2024-10-29 03:37:02 -0400

    Chat implementation + frontend work

--- frontend/src/pages/MessagesPage.jsx
+++ frontend/src/pages/MessagesPage.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {useContext} from "react";
+import {ChatContext} from "../context/ChatContext";
+import { useUser } from "../context/UserContext";
+import UserChat from "../components/ChatComponents/UserChat";
+import ChatBox from "../components/ChatComponents/ChatBox";
 
 const MessagesPage = () => {
-  return (
-    <div>MessagesPage</div>
-  )
+    //const {user} = useContext(AuthContext);
+    const user = {
+      _id: '671ff74a17e6c584dddf74ee',
+      firstName: 'Carson',
+      lastName: 'Chang',
+      email: 'cadchang@gmail.com',
+      profilePicture: 'https://avatars.githubusercontent.com/u/136373179?v=4',
+      listings: 5,
+      rating: 4.5,
+    };
+
+
+    const {userChats, isUserChatsLoading, updateCurrentChat} = useContext(ChatContext);
+
+    return <section className="p-5">
+        {userChats?.length < 1 ? null : (
+            <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="col-span-1">
+                    {isUserChatsLoading && <p>Loading conversation...</p>}
+                    {userChats?.map((chat, index) => {
+                        return (
+                            <div key={index} onClick={() => updateCurrentChat(chat)}>
+                                <UserChat chat={chat} user={user}/>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="col-span-1">
+                    <ChatBox/>
+                </div>
+            </section>
+        )}
+    </section>
 }
 
 export default MessagesPage
