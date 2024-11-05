ae6607e0ee0f1ca01337fbb4950a832f6e89a0ad
Carson Chang <gimchang30@gmail.com>
2024-11-05 18:30:57 -0500

    Major UI rework. Changed main theme and secondary color, standardized buttons, homepage and profile page header reworks.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,14 +11,14 @@ const Navbar = () => {
 
   return (
     <div className='fixed inset-x-0 bottom-6 flex justify-center'>
-      <div className='flex flex-row justify-center space-x-8 backdrop-blur-md bg-opacity-30 bg-dark-blue w-[365px] rounded-full py-2 border border-white border-opacity-40'>
+      <div className='flex flex-row justify-center space-x-8 backdrop-blur-md bg-opacity-50 bg-[#1F1F1F] w-[365px] rounded-full py-2 border border-white border-opacity-40'>
         <div className={location.pathname === '/create' ? 'p-1 rounded-full bg-white' : 'p-1 rounded-full border-transparent'}>
           <TagIcon color={location.pathname === '/create' ? '#1A1E26' : 'white'} className='w-[35px] relative z-10 top-1' onClick={() => navigate('/create')} />
         </div>
         <div className={location.pathname === '/search' ? 'px-[9px] py-2  rounded-full bg-white' : 'px-[9px] py-2 rounded-full border-transparent'}>
           <SearchIcon color={location.pathname === '/search' ? '#1A1E26' : 'white'} className='w-[35px] relative z-10' onClick={() => navigate('/search')} />
         </div>
-        <div className={location.pathname === '/home' ? 'p-1 rounded-full bg-white' : 'p-1 rounded-full border-transparent'}>
+        <div className={location.pathname === '/home' ? 'p-1 rounded-full bg-white items-center justify-center' : 'p-1 rounded-full border-transparent'}>
           <HomeIcon color={location.pathname === '/home' ? '#1A1E26' : 'white'} className='w-[35px] relative z-10' onClick={() => navigate('/home')} />        
         </div>
         <div className={location.pathname === '/messages' ? ' px-[7px] py-[5px] rounded-full bg-white' : 'px-[7px] py-[5px] rounded-full border-transparent'}>
