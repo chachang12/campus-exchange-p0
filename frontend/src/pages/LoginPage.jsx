10dc1913b18cb08790ed8ce736c8918b8a796227
Carson Chang <136373179+chachang12@users.noreply.github.com>
2024-11-05 15:09:36 -0500

    Minor changes to the login UI.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import { googlelogo } from '../assets';
-import './WelcomePage.css'; 
+import React from 'react';
+import { Link } from 'react-router-dom';
 import CXLogo from '../components/icons/logo';
+import './WelcomePage.css'; 
+import googlelogo from '../assets/google.png';
 
-export const LoginPage = () => {
+const LoginPage = () => {
 
     const handleLogin = () => {
         window.location.href = 'http://localhost:8080/auth/google';
     };
 
     return (
-        <div className="flex flex-col items-center justify-between min-h-screen custom-bg">
-            <div className='my-20'>
+        <div className='flex flex-col items-center min-h-screen custom-bg justify-between text-white'>
+            <div className='mt-20 w-[80%]'>
                 <CXLogo className='' width="100" height="100" fill="white" />
-            </div>
-
-            <div className='bg-white w-full flex-grow rounded-t-[40px] backdrop-blur-md flex flex-col items-center justify-start bg-opacity-5'>
-                <h1 className='text-white text-3xl mt-20 mb-6 weight font-semibold'>
-                    Sign In
-                </h1>
-                <div className="">
-                    <button onClick={handleLogin} className="">
-                        <img src={googlelogo} alt="google" className="w-20 h-20" />
-                    </button>
+                <div>
+                    <h1 className='text-3xl mt-4'>Sign In</h1>
+                    {/* <h1 className='text-3xl'>Campus Exchange</h1> */}
                 </div>
+            </div>
+            <div className='flex flex-row gap-2 mb-20'>
+                <button onClick={handleLogin} className='flex flex-row gap-2 border-white px-8 py-4 rounded-full border-[0.5px]'>
+                    <img src={googlelogo} alt="google" className="w-6 h-6" />
+                    <h1>Login with Google</h1>
+                </button>
             </div>
         </div>
     );
