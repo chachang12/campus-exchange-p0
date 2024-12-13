76a2f352201f8fca6bb40c9aa1598c4baa4072e6
Carson Chang <cadchang@gimchang30@gmail.com>
2024-12-13 03:52:55 -0500

    wtv atp

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ app.use(passport.session());
 app.use(cors({
   origin: process.env.CLIENT_BASE_URL,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
