3d4e4727af1e1da5e0fe6fcaad423f4f9fc12879
Carson Chang <cadchang@gimchang30@gmail.com>
2024-12-13 13:30:11 -0500

    aws changes

--- backend/config/awsConfig.js
+++ backend/config/awsConfig.js
@@ -1,7 +1,22 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env.production' });
+dotenv.config();
+
+// Determine the environment
+const ENV = process.env.NODE_ENV || 'development';
+
+// Set the path to the appropriate .env file within the backend directory
+let envFile = './.env.development';
+if (ENV === 'production') {
+  envFile = './.env.production';
+}
+
+// Load environment variables from the specified .env file
+dotenv.config({ path: envFile });
+
+console.log(`Running in ${ENV} mode`);
+console.log('Loaded MONGO_URI:', process.env.MONGO_URI);
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
