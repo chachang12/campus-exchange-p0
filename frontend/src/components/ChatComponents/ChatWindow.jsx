624c45120dc76a678ecc537fd82425eb854b50f1
caljshim <caljshim@gmail.com>
2025-02-04 18:41:19 -0500

    Responsive design for messages page

--- frontend/src/components/ChatComponents/ChatWindow.jsx
+++ frontend/src/components/ChatComponents/ChatWindow.jsx
@@ -13,7 +13,6 @@ const ChatWindow = () => {
   const { chatId } = useParams();
   const { recipientUser } = useFetchRecipientUser(currentChat, user);
   const [textMessage, setTextMessage] = useState("");
-  const navigate = useNavigate();
   const messagesEndRef = useRef(null);
 
   let productData;
@@ -24,61 +23,61 @@ const ChatWindow = () => {
   }
 
   useEffect(() => {
+    if (userChats && window.innerWidth > 640) {
+      updateCurrentChat(userChats[0])
+    }
+  })
+
+  useEffect(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-      const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-      document.documentElement.style.width = `${w}px`;
-      document.documentElement.style.height = `${h}px`;
-      document.body.style.width = `${w}px`;
-      document.body.style.height = `${h}px`;
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize(); // Initial call to set the dimensions
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  return isMessagesLoading ? (
-    <div className="text-center text-white">
-      Loading messages...
-    </div>
-  ) : (
-    <section className="flex flex-col h-screen min-h-[700px] items-center p-4">
-      <section className="fixed top-0 w-full z-10">
-        <ChatHeader />
-      </section>
-      <section id="chat-box" className="flex-1 overflow-y-auto w-full pt-24 pb-20">
-        {messages && messages.map((message, index) => (
-          <div key={index} className={`mb-4 ${message?.senderId === user?._id ? "flex justify-end" : "flex justify-start"}`}>
-            <div className={`flex flex-col w-9/12 ${message?.senderId === user?._id ? "items-end" : "items-start"}`}>
-              <div className={`inline-block s:max-w-md px-4 py-2 rounded-2xl ${message?.senderId === user?._id ? "bg-blue-500 text-white" : "bg-gray-700 text-white"}`}>
-                {message.text}
-              </div>
-              <div className="text-gray-400 text-xs mt-1">
-                {moment(message.createdAt).calendar({ sameDay: 'h:mm A', lastDay: '[Yesterday]', lastWeek: 'MMM D', sameElse: 'MMM D, YYYY' })}
+  if (isMessagesLoading) {
+    return (
+      <p className="text-2xl flex items-center justify-center text-center font-bold text-darkgray mt-4 h-3/4 m:hidden">
+        Loading messages...
+      </p>
+    )
+  }
+  else if (!currentChat) {
+    return (
+      <p className="text-2xl flex items-center justify-center text-center font-bold text-darkgray mt-4 h-3/4">
+        No chat selected.
+      </p>
+    )
+  }
+  else {
+    return (
+      <section className="flex flex-col w-full h-screen min-h-[700px] items-center p-4">
+        <section className="top-0 w-full z-10">
+          <ChatHeader/>
+        </section>
+        <section id="chat-box" className="flex-1 overflow-y-auto w-full pt-8 pb-20">
+          {messages && messages.map((message, index) => (
+            <div key={index} className={`mb-4 ${message?.senderId === user?._id ? "flex justify-end" : "flex justify-start"}`}>
+              <div className={`flex flex-col w-9/12 ${message?.senderId === user?._id ? "items-end" : "items-start"}`}>
+                <div className={`inline-block s:max-w-md px-4 py-2 rounded-2xl ${message?.senderId === user?._id ? "bg-blue-500 text-white" : "bg-gray-700 text-white"}`}>
+                  {message.text}
+                </div>
+                <div className="text-gray-400 text-xs mt-1">
+                  {moment(message.createdAt).calendar({ sameDay: 'h:mm A', lastDay: '[Yesterday]', lastWeek: 'MMM D', sameElse: 'MMM D, YYYY' })}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-        <div ref={messagesEndRef} />
+          ))}
+          <div ref={messagesEndRef} />
+        </section>
+        <section className="chat-input p-2 flex items-center bottom w-[95%] mb-2 border border-white border-opacity-20 rounded-full bg-[#1A1E26] backdrop-blur-md bg-opacity-30">
+          <input className="flex-1 px-2 py-2 rounded-full bg-inherit text-white outline-none" placeholder="Message" type="text" value={textMessage} onChange={(e) => setTextMessage(e.target.value)} />
+          <button className="ml-4 p-2 bg-blue-500 text-white rounded-full" onClick={() => sendTextMessage(textMessage, user, currentChat._id, setTextMessage)}>
+            <FaArrowUp />
+          </button>
+        </section>
       </section>
-      <section className="chat-input p-2 flex items-center fixed bottom-0 w-[95%] mb-2 border border-white border-opacity-20 rounded-full bg-[#1A1E26] backdrop-blur-md bg-opacity-30">
-        <input className="flex-1 px-2 py-2 rounded-full bg-inherit text-white outline-none" placeholder="Message" type="text" value={textMessage} onChange={(e) => setTextMessage(e.target.value)} />
-        <button className="ml-4 p-2 bg-blue-500 text-white rounded-full" onClick={() => sendTextMessage(textMessage, user, currentChat._id, setTextMessage)}>
-          <FaArrowUp />
-        </button>
-      </section>
-    </section>
-  );
-};
+    )
+  }
+}
 
 export default ChatWindow;
--- frontend/src/pages/MessagesPage.jsx
+++ frontend/src/pages/MessagesPage.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ChatContext } from "../context/ChatContext";
 import { useUser } from "../context/UserContext";
 import UserChat from "../components/ChatComponents/UserChat";
+import ChatWindow from '../components/ChatComponents/ChatWindow';
 import { useNavigate } from 'react-router-dom';
 
 const MessagesPage = () => {
     const { user } = useUser();
-    const { userChats, isUserChatsLoading, updateCurrentChat, setUserChats, notifications, newMessage } = useContext(ChatContext);
+    const { userChats, isUserChatsLoading, updateCurrentChat, currentChat, notifications, newMessage } = useContext(ChatContext);
     const navigate = useNavigate();
+    const [isChatExpanded, setIsChatExpanded] = useState(false);
     const handleChatClick = (chat) => {
         updateCurrentChat(chat);
-        navigate(`/chat/${chat._id}`);
     };
 
     useEffect(() => {
-        // This useEffect will run whenever `notifications` changes
-        console.log("Notifications updated:", notifications);
-        // Optionally, you can trigger specific logic here
-    }, [notifications, newMessage]);
+        if (currentChat) {
+            setIsChatExpanded(true)
+        }
+        else {
+            setIsChatExpanded(false)
+        }
+    }, [currentChat])
 
     return (
-        <section className="flex flex-col h-screen">
+        <section className="grid h-screen grid-cols-1 sm:grid-cols-[30%_70%]">
             <div className="w-full overflow-y-auto">
                 <h2 className="text-white text-xl font-semibold mb-4 text-center pt-4">Messages</h2>
                 {isUserChatsLoading ? (
@@ -38,6 +42,9 @@ const MessagesPage = () => {
                         </div>
                     )
                 }
+            </div>
+            <div className={`w-full text-white m:${isChatExpanded ? "" : "hidden"}`}>
+                <ChatWindow />
             </div>
         </section>
     );
